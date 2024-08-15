b73c7f523a947ccf21d9fc0dea6d332474544db1
Arin7712 <aringawande7712@gmail.com>
2024-08-16 00:50:21 +0530

    uhudwsdjs

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -17,7 +17,7 @@ const handler = NextAuth({
         })
     ], 
 
-    callbacks: {
+    callback: {
         async session ({session}) {
             const sessionUser = await User.findOne({
                 email: session.user.email,
