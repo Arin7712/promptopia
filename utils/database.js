8a854ed67676b81ed6a661231e3bda88e1b5c5e3
Arin7712 <aringawande7712@gmail.com>
2024-08-16 00:21:20 +0530

    ufb

--- utils/database.js
+++ utils/database.js
@@ -20,6 +20,7 @@ export const connectToDB = async() => {
         })
 
         isConnected = true;
+        console.log('MongoDB Connected')
     } catch (error) {
         console.log(error)
     }
