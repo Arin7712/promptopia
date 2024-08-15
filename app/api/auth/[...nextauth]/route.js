5b6c365c5bb23d33c92bf3abc7e42f7e99948b72
Arin7712 <aringawande7712@gmail.com>
2024-08-15 23:31:12 +0530

    Added elon

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
 import {connectToDB} from '@utils/database'
-import {connetToDB} from '@utils/database'
 import User from "@models/user";
 
 console.log({
