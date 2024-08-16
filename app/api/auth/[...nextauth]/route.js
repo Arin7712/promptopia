28d6e1606c4562cff5860c1906b2f0dfa2785139
Arin7712 <aringawande7712@gmail.com>
2024-08-17 01:46:10 +0530

    ihdijsiiswis

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,12 +11,13 @@ const handler = NextAuth({
   callbacks: {
     async session({ session }) {
       // You can add any session logic here if needed
+      console.log('fkas');
       return session;
     },
     async signIn({ account, profile, user, credentials }) {
       try {
         // You can add any sign-in logic here if needed
-        console.log('fk')
+        console.log('fk');
         return true;
       } catch (error) {
         console.log("Error during sign-in: ", error.message);
