9fc21f7eb0b389d476558fbf9b444d2e30140805
Arin7712 <aringawande7712@gmail.com>
2024-08-17 01:40:56 +0530

    jdbhsbdjsj

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-import User from '@models/user';
-import { connectToDB } from '@utils/database';
-
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -12,42 +9,20 @@ const handler = NextAuth({
     })
   ],
   callbacks: {
-    
     async session({ session }) {
-      // store the user id from MongoDB to session
-      console.log('checking33')
-      const sessionUser = await User.findOne({ email: session.user.email });
-      session.user.id = sessionUser._id.toString();
-      console.log('checking')
+      // You can add any session logic here if needed
       return session;
     },
     async signIn({ account, profile, user, credentials }) {
       try {
-        await connectToDB();
-        console.log('checking1')
-
-        // check if user already exists
-        const userExists = await User.findOne({ email: profile.email });
-
-        // if not, create a new document and save user in MongoDB
-        if (!userExists) {
-          await User.create({
-            email: profile.email,
-            username: profile.name.replace(" ", "").toLowerCase(),
-            image: profile.picture,
-          });
-          console.log('checking2')
-        }
-
-        console.log('checking6')
-
-        return true
+        // You can add any sign-in logic here if needed
+        return true;
       } catch (error) {
-        console.log("Error checking if user exists: ", error.message);
-        return false
+        console.log("Error during sign-in: ", error.message);
+        return false;
       }
     },
   }
-})
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
